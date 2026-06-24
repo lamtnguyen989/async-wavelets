c82daa1ec079e1887acbcc9b3f7bcce5f7ff709f
Lam Nguyen <lamtnguyen989@gmail.com>
2026-06-24 17:24:34 -0500

    Math details

--- src/wavelet.rs
+++ src/wavelet.rs
@@ -4,6 +4,13 @@ use num_complex::{Complex64};
 *   Generalized Morse Wavelet
 ***/
 
+/// Normalization constant metric context
+ #[derive(Clone, Copy)]
+pub enum Normalization{
+    L1, // Convention to use this
+    L2
+}
+
 /// Parameters deciding which wavelet within the Generalized Morse Wavelet family
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct GMW_Params
@@ -26,14 +33,30 @@ impl GMW_Params
     pub fn get_beta(&self) -> f64 {return self.beta;}
     pub fn get_gamma(&self) -> f64 {return self.gamma;}
 
-    /// Peak frequency calculation helper
+    /// Peak frequency of GMW based on the parameters
     pub fn peak_freq(&self) -> f64 {return (self.beta/self.gamma).powf(1.0/self.gamma);}
+
+    /// Calculate normalization constant based on metric context
+    pub fn normalization_const(&self, norm_type: Normalization) -> f64 {
+        let (beta, gamma): (f64, f64) = (self.beta, self.gamma);
+
+        match norm_type {
+            Normalization::L1 => {
+                let c = 2.0;    // Convention
+                return c * (std::f64::consts::E*gamma / beta).powf(beta/gamma);
+            },
+            Normalization::L2 => {
+                todo!("Calculate L2 normalization constant");
+            },
+        }
+    }
 }
 
-/// Normalization constant metric context
-pub enum Normalization{
-    L1, // Convention to use this
-    L2
+impl From<(f64, f64)> for GMW_Params {
+    fn from(tuple: (f64, f64)) -> Self {
+        let (beta, gamma) = (tuple.0, tuple.1);
+        return GMW_Params::new(beta, gamma);
+    }
 }
 
 /// Actual Generalized Morse wavelet handler
@@ -47,33 +70,21 @@ pub struct GeneralizedMorseWavelet
 impl GeneralizedMorseWavelet
 {
     /// Constructors
-    pub fn new(beta: f64, gamma: f64, norm_type: Normalization) -> Self {
-        let params = GMW_Params::new(beta, gamma);
+    pub fn new<P>(params: P, norm_type: Normalization) -> Self
+    where P: Into<GMW_Params>
+    {
+        let params: GMW_Params = params.into();
         return Self {
             params: params,
-            alpha: Self::calculate_normalization_const(&params, norm_type)
+            alpha:  params.normalization_const(norm_type)
         }
     }
 
-    pub fn new_with_param(params: GMW_Params, norm_type: Normalization) -> Self {
-        return Self {
-            params: params,
-            alpha: Self::calculate_normalization_const(&params, norm_type)
+    /// Wavelet transform coefficient value via convolution in frequency space
+    pub fn value(&self, omega: f64, scale: f64) -> Complex64 {
+        if omega <= 0.0 {
+            return Complex64::new(0.0, 0.0);
         }
-    }
-
-    /// Calculate normalization constant
-    fn calculate_normalization_const(param: &GMW_Params, norm_type: Normalization) -> f64 {
-        let (beta, gamma): (f64, f64) = (param.get_beta(), param.get_gamma());
-
-        match norm_type {
-            Normalization::L1 => {
-                let c = 2.0;    // Convention
-                return c * (std::f64::consts::E*gamma / beta).powf(beta/gamma);
-            },
-            Normalization::L2 => {
-                todo!("Calculate L2 normalization constant");
-            },
-        }
+        todo!();
     }
 }
