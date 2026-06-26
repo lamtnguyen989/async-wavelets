727973e5a92fbbd432ada90ae02f363b83f6621e
Lam Nguyen <lamtnguyen989@gmail.com>
2026-06-25 19:36:42 -0500

    cli

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,16 @@ struct Args
     data: PathBuf,
 
     // Beta parameters
-    #[arg(short, long, value_delimiter=',')]
+    #[arg(short, long, value_delimiter=',', default_value="3")]
     beta: Vec<f64>,
 
     // Gamma parameters
-    #[arg(short, long, value_delimiter=',')]
+    #[arg(short, long, value_delimiter=',', default_value="3")]
     gamma: Vec<f64>,
+
+    // Normalization metric
+    #[arg(short, long, default_value="L1")]
+    metric: String,
 
     // Min frequency considered for scalogram (in Hz)
     #[arg(long, default_value_t=20.0)]
@@ -39,7 +43,19 @@ async fn say_hello() -> () {
 /// Entry point
 #[tokio::main]
 async fn main() -> Result<()> {
+
+    // Processing CLI args data
     let args = Args::parse();
+    let norm_metric = match args.metric.as_str() {
+        "L1" => Normalization::L1,
+        "L2" => Normalization::L2,
+        _ => {println!("Unknown metric context, default to L1"); Normalization::L1}
+    };
+
+    let family: Vec<GeneralizedMorseWavelet> = args
+        .beta.iter().zip(args.gamma.iter())
+        .map(|(&b, &g)| {GeneralizedMorseWavelet::new((b, g), norm_metric)})
+        .collect();
 
     say_hello().await;
     
