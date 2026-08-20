cc0f684971c6f614c2a9b604aff9d7e5d10bf061
Lam Nguyen <lamtnguyen989@gmail.com>
2026-08-19 21:31:36 -0500

    setting up audio decoding pipeline again

--- engine/src/audio.rs
+++ engine/src/audio.rs
@@ -1,5 +1,6 @@
 use anyhow::{Result, Context};
 use std::path::{Path, PathBuf};
+use std::todo;
 
 use symphonia::core::codecs::{CodecParameters};
 use symphonia::core::codecs::registry::{CodecRegistry};
@@ -16,4 +17,28 @@ pub struct DecodedAudio
 {
     pub samples: Vec<f32>,
     pub sample_rate: u32,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum DecodeError 
+{
+    #[error("Could not recognize this as a supported audio format (MP3, FLAC, or WAV): {0}")]
+    UnrecognizedFormat(String),
+    #[error("No playable audio track found in file")]
+    NoAudioTrack,
+    #[error("Unsupported audio codec: {0}")]
+    UnsupportedCodec(String),
+    #[error("sample rate could not be determined")]
+    UnknownSampleRate,
+    #[error("Demuxing error: {0}")]
+    Demux(String),
+    #[error("Decoding error: {0}")]
+    Decode(String),
+    #[error("No audio samples in the decoding")]
+    EmptyAudio,
+}
+
+pub fn decode_audio(bytes: Vec<u8>) -> Result<DecodedAudio, DecodeError>
+{
+    todo!();
 }
